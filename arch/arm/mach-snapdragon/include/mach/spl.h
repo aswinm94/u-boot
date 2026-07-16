54b513b422f9294e4eae5af389e5dd06843e2843
Varadarajan Narayanan <varadarajan.narayanan@oss.qualcomm.com>
2026-07-16 16:39:38 +0530

    mach-snapdragon: spl: ipq5210: Add SPL support
    This patch adds the basic board init routines that invoke the common and
    mach-snapdragon specific SPL frameworks to proceed with the hand over from
    boot rom to the next stage i.e. U-Boot proper. This also provides the
    linker script to generate the SPL image in the format expected by the boot
    rom.
    
    Reviewed-by: Balaji Selvanathan <balaji.selvanathan@oss.qualcomm.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Varadarajan Narayanan <varadarajan.narayanan@oss.qualcomm.com>

---
+++ arch/arm/mach-snapdragon/include/mach/spl.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef __SPL_H__
+#define __SPL_H__
+
+void qcom_spl_malloc_init_f(void);
+int qcom_spl_loader_pre_ddr(u8 boot_device);
+void qcom_spl_error_handler(void *arg);
+
+#endif /* __SPL_H__ */
