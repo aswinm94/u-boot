8729be53896b311f6a50f9d3b2592a44b4b5562b
Varadarajan Narayanan <varadarajan.narayanan@oss.qualcomm.com>
2026-09-17 13:15:35 +0530

    mach-snapdragon: Kconfig: Add rules to create SPL images
    The boot rom expects the secondary boot loader in MBN format followed by
    the TME executable. Add rules to board/qualcomm/config.mk to create
    u-boot-spl.mbn and u-boot-spl.melf.
    
    u-boot-spl.melf is the combined multi-elf file that has u-boot-spl.mbn
    followed by the TME executable.
    
    Additionally, this patch adds a config option to specify the path
    of the TME executable to be used to create u-boot-spl.melf.
    
    Signed-off-by: Varadarajan Narayanan <varadarajan.narayanan@oss.qualcomm.com>

--- board/qualcomm/config.mk
+++ board/qualcomm/config.mk
@@ -8,7 +8,30 @@
 # Create Qualcomm signed elf images
 CMD_MKMBN = $(srctree)/tools/qcom/mkmbn/mkmbn.py
 quiet_cmd_mkmbn = MBN     $@
-      cmd_mkmbn = $(CMD_MKMBN) $<
+      cmd_mkmbn = $(CMD_MKMBN) -l $(CONFIG_TEXT_BASE) $<
 
 u-boot.mbn: u-boot.bin FORCE
 	$(call if_changed,mkmbn)
+
+ifeq ($(CONFIG_QCOM_GENERATE_MBN),y)
+
+quiet_cmd_mksplmbn = SPLMBN     $@
+      cmd_mksplmbn = $(CMD_MKMBN) -o spl/u-boot-spl.mbn -l $(CONFIG_SPL_TEXT_BASE) $<
+
+INPUTS-$(CONFIG_SPL) += spl/u-boot-spl.mbn
+
+spl/u-boot-spl.mbn: spl/u-boot-spl.bin FORCE
+	$(call if_changed,mksplmbn)
+
+ifneq ($(wildcard $(CONFIG_QCOM_TMEL_ELF)),)
+
+quiet_cmd_mksplmelf = SPLMELF     $@
+      cmd_mksplmelf = $(CMD_MKMBN) -o spl/u-boot-spl.melf spl/u-boot-spl.mbn $(CONFIG_QCOM_TMEL_ELF)
+
+INPUTS-$(CONFIG_SPL) += spl/u-boot-spl.melf
+
+spl/u-boot-spl.melf: spl/u-boot-spl.mbn FORCE
+	$(call if_changed,mksplmelf)
+endif	# CONFIG_QCOM_TMEL_ELF
+
+endif	# CONFIG_QCOM_GENERATE_MBN
